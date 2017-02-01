ec4fa309e55f8710f3bd34f969cd220afe40ae32
kang hwan ki <xangfi@gmail.com>
2017-02-01 14:58:19 +0900

    Update pipes.ts

--- src/common/pipes.ts
+++ src/common/pipes.ts
@@ -1,1 +1,12 @@
 import { Pipe } from '@angular/core';
+import { PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'XXX'
+})
+export class XXX implements PipeTransform {
+  transform(o: any, ...arg: any[]): any{
+    // todo
+    return o;
+  }
+}
